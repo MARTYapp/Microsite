f5ff8ef07f2ce830f3dfef8a7414ea63ed0d1b4d
MARTYapp <ericdanieladler@gmail.com>
2025-07-23 11:46:46 -0400

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      keyframes: {
-        ripple: {
-          '0%': { opacity: '1', transform: 'scale(0.5)' },
-          '100%': { opacity: '0', transform: 'scale(4)' },
-        },
-      },
-      animation: {
-        ripple: 'ripple 0.8s ease-out forwards',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
