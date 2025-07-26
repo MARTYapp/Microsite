3cff3299a4060b5a8a0bec965149d2552cb1f50c
MARTYapp <ericdanieladler@gmail.com>
2025-07-26 12:18:52 -0400

    chore(config): added allowedDevOrigins for local dev

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    allowedDevOrigins: ["http://127.0.0.1:3000"],
+  },
   // output: 'export', ← 🔥 DO NOT USE this with middleware
 };
 
