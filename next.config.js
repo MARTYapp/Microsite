5aff69949a79a8b949a032d50343003f582a6629
MARTYapp <ericdanieladler@gmail.com>
2025-07-28 03:06:39 -0400

    righta

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // required for `npx next export`
   reactStrictMode: true,
   images: {
     unoptimized: true, // required if using <Image> with static export
+  },
+  experimental: {
+    appDir: true, // enables App Router (dynamic SSR/ISR supported)
   },
 }
 
