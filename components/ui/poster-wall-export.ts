cc4511be00ea59ae40b5a71e1f370a8fc5d14283
MARTYapp <ericdanieladler@gmail.com>
2025-07-29 00:24:27 -0400

    PUSH

--- components/ui/poster-wall-export.ts
+++
@@ -1,1 +0,0 @@
-export { default as PosterWall } from './poster-wall-export'
---
+++ components/ui/poster-wall-export.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+
+export const PosterWall = () => {
+  return (
+    <div className="text-white">
+      <h2 className="text-2xl font-bold mb-4">Poster Wall</h2>
+      <p className="text-white/70">More MARTY merch coming soon…</p>
+    </div>
+  )
+}
