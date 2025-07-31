9b9dac4f4de8bad324e37c454a336e656dd072a2
MARTYapp <ericdanieladler@gmail.com>
2025-07-31 03:06:03 -0400

    tton

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,16 @@
 'use client'
 import React from 'react'
 
-export const Button = ({ children }: { children: React.ReactNode }) => (
-  <button className="bg-blue-600 text-white px-4 py-2 rounded">{children}</button>
+type ButtonProps = {
+  children: React.ReactNode
+  className?: string
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button = ({ children, className = '', ...props }: ButtonProps) => (
+  <button
+    className={`bg-blue-600 text-white px-4 py-2 rounded ${className}`}
+    {...props}
+  >
+    {children}
+  </button>
 )
