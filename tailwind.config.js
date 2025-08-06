fe2ad73f716dee6d43082b27eab86a677cb4ef46
MARTYapp <ericdanieladler@gmail.com>
2025-08-06 11:43:36 -0400

    jh

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,11 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        'marty-bg': '#0A0A0A',
-        'marty-text': '#F5F5F5',
-        'marty-accent': '#FF5E5B'
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif']
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
