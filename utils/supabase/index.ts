b17e0869390b666c74a8cd3595a989ac05658af8
MARTYapp <ericdanieladler@gmail.com>
2025-08-21 22:45:20 -0400

    sac

---
+++ utils/supabase/index.ts
@@ -0,0 +1,3 @@
+export { createClient as createBrowserClient } from './client'
+export { createClient as createserverClient } from './server'
+export { createClient as createMiddlewareClient } from './middleware'
