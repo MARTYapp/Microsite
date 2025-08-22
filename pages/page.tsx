5f25ca6688f79067ccb062966b92cf1d9f4423cf
MARTYapp <ericdanieladler@gmail.com>
2025-08-21 22:15:46 -0400

    sav

--- pages/page.tsx
+++ pages/page.tsx
@@ -3,14 +3,16 @@ import { createClient } from '../utils/supabase/server'
 
 export default async function Page() {
   const supabase = createClient()
-  const supabase = createClient()
 
-  const { data: todos } = await supabase.from('todos').select()
+  const { data: todos, error } = await supabase.from('todos').select('*')
+  if (error) {
+    return <div>Failed to load todos: {error.message}</div>
+  }
 
   return (
     <ul>
-      {todos?.map((todo) => (
-        <li>{todo}</li>
+      {todos?.map((todo: any) => (
+        <li key={todo.id}>{todo.title ?? todo.text ?? JSON.stringify(todo)}</li>
       ))}
     </ul>
   )
